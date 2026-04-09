baf16507867ec3b9ba22eaaefbf5d29c9ec28a5f
wangtim9 <wangtim27@7hills.org>
2026-04-09 09:35:00 -0400

    Update VocablList.java

--- VocablList.java
+++ VocablList.java
@@ -4,12 +4,34 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class VocabList() {
-
-}
-class Terms() {
-
-}
-
-class Definitions() {
-
-}
+	public void makeQuestion(ArrayList<String> termlist, ArrayList<String> deflist) {
+		int temp = Math.random() * termlist
+		ArrayList<String> options = new ArrayList<String>(4);
+		options.add(deflist.get(Math.random()*deflist.size());
+		options.add(deflist.get(Math.random()*deflist.size());
+		options.add(deflist.get(Math.random()*deflist.size());
+		options.add(deflist.get(temp);
+		while (
+	public static void Main(String[] args) {
+		ArrayList<String> terms;
+		ArrayList<String> defs;
+		File Terms = new File("unit1terms.txt");
+		try (Scanner Scan = new Scanner(Terms) {
+			while (Scan.hasNextLine()) {
+				String term = Scan.nextLine();
+				terms.add(term);
+			}
+		}
+		File Defs = new File("unit1defs.txt");
+		try Scanner Scan = new Scanner(Defs) {
+			while (Scan.hasNextLine()) {
+				String def = Scan.nextLine();
+				defs.add(def);
+			}
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("File not Found");
+		}
+		System.out.println(terms.get(Math.random() * terms.size());
+		
+         }
